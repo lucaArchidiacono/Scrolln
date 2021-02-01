1adb063a5f8edac9f7297de9eb75269e656b3590
lucaArchidiacono <lucaarchidiacono@gmail.com>
2021-02-01 17:27:30 +0100

    Initial setup to print added/removed devices

--- Scrolly/ContentView.swift
+++ Scrolly/ContentView.swift
@@ -2,18 +2,32 @@
 //  ContentView.swift
 //  Scrolly
 //
-//  Created by Luca Archidiacono on 01.02.21.
+//  Created by Luca Archidiacono on 31.01.21.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    let example = usbDelegate()
     var body: some View {
         Text("Hello, World!")
             .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
 
+class usbDelegate: USBWatcherDelegate {
+    private var usbWatcher: USBWatcher!
+    
+    init() {
+        usbWatcher = USBWatcher(delegate: self)
+    }
+    internal func deviceAdded(_ device: io_object_t) {
+        print("device added: \(device.name() ?? "<unknown>")")
+    }
+    internal func deviceRemoved(_ device: io_object_t) {
+        print("device removed: \(device.name() ?? "<unknown>")")
+    }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ Scrolly/extensions/IOObjectExtension.swift
@@ -0,0 +1,22 @@
+//
+//  IOObjectExtension.swift
+//  Scrolly
+//
+//  Created by Luca Archidiacono on 31.01.21.
+//
+
+import Foundation
+
+extension io_object_t {
+    /// - Returns: The device's name.
+    func name() -> String? {
+        let buf = UnsafeMutablePointer<io_name_t>.allocate(capacity: 1)
+        defer { buf.deallocate() }
+        return buf.withMemoryRebound(to: CChar.self, capacity: MemoryLayout<io_name_t>.size) {
+            if IORegistryEntryGetName(self, $0) == KERN_SUCCESS {
+                return String(cString: $0)
+            }
+            return nil
+        }
+    }
+}
