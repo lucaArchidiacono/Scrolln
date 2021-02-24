57429f52dbe2dfbe8ed2e8f2ac7a75b6489a3e0f
lucaArchidiacono <lucaarchidiacono@gmail.com>
2021-02-24 20:06:17 +0100

    renamed app from Scrolly to Scrolln

--- ScrollnTests/ScrollnTests.swift
+++ ScrollnTests/ScrollnTests.swift
@@ -6,9 +6,9 @@
 //
 
 import XCTest
-@testable import Scrolly
+@testable import Scrolln
 
-class ScrollyTests: XCTestCase {
+class ScrollnTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
